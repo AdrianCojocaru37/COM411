d2fe7faf3e890ca88e40440e12bc5320fcf9b2c5
repl.it user <replituser@example.com>
2021-02-26 11:33:38 +0000

    Message read

---
+++ basics/output/escape_characters.py
@@ -0,0 +1,7 @@
+
+
+print("\n Dispalys a new line")
+print("\t Displays a tab space")
+print("\\ Dispalys a backslash")
+print("\" Displays double quote")
+print("\' Displays single quote'")
---
+++ basics/output/multi_message.py
@@ -0,0 +1,7 @@
+
+print("System Failure Imminent!")
+print("System Reboot has been initiated...\n")
+print("...rebooting sensory system")
+print("...rebooting output motors")
+print("...rebooting hover engine\n")
+print("System online")
