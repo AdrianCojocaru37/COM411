1ae6b5ee2f20ed80ba7ef5da70eace0e7a5d149f
repl.it user <replituser@example.com>
2021-03-18 23:54:01 +0000

    reversing

--- basics/repetitions/for_loop/reverse.py
+++ basics/repetitions/for_loop/reverse.py
@@ -1,15 +1,7 @@
 
-
-
 print("What phrase do you see?")
 phrase = input()
-
-
 print("\nReversing...\nThe phrase is", end="")
 
-reversed = ""
-
-for letter in phrase:
-    reversed = letter + reversed
-
-print(reversed)
+for position in range(len(phrase) - 1, -1, -1):
+    print(phrase[position], end="")
