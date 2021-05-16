ef8fe8ec7bac0cf04a98564fd97711d67e5a5070
repl.it user <replituser@example.com>
2021-05-16 23:08:34 +0000

    test json files

--- oop/robot.py
+++ oop/robot.py
@@ -1,10 +1,10 @@
-class Human:
+class Robot:
   MAX ENERGY =100
 
   def __init__(self,name=, age=0, energy=Human.MAX_ENERGY):
     self.name="Human"
     self.age=0
-    self.energy =Human.MAX_ENERGY
+    self.energy =0
 
 
   def display(self):
